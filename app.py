3f307320f934569f1a446e7d36feb9c2ebbea7b3
Valerie Forest <valerie.forest-guillaumeaud@u-bordeaux.fr>
2025-12-05 06:53:45 +0100

    Fix Vercel Flask structure

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-from flask_app.app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/MEMORY")
+def MiniJeu():
+    return render_template("JEU.html")
+
+@app.route("/LINUX")
+def linux():
+    return render_template("LINUX.html")
+
+@app.route("/PARTI-PRIS")
+def PartiPri():
+    return render_template("PartiPris.html")
+
+@app.route("/FRISE")
+def Frise():
+    return render_template("frise.html")
+
+# ✅ OBLIGATOIRE POUR VERCEL
+handler = app
